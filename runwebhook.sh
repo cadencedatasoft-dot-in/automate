9de29c9d520969402caaf761e9d24387ca8a1b9b
Cadence Data Soft <banand@cadencedatasoft.in>
2022-09-27 18:59:01 +0530

    Run using python3

--- runwebhook.sh
+++ runwebhook.sh
@@ -1,3 +1,3 @@
 # // 9000 ec2-54-185-122-8.us-west-2.compute.amazonaws.com 9000 anand 12345cds
 # // webhook server port, minio host, minio port, minio user, minio pass
-python app.py $1 $2 $3 $4 $5
+python3 app.py $1 $2 $3 $4 $5
