beda722723e829f1b827b0d597375c87f18de36b
Cadence Data Soft <banand@cadencedatasoft.in>
2022-09-27 17:23:59 +0530

    Silent installation

--- prerequisites.sh
+++ prerequisites.sh
@@ -5,4 +5,5 @@ sudo apt-get -y install openssl
 sudo apt-get -y install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
 sudo apt-get -y install python
 sudo apt-get -y install pip
+sudo apt-get -y install python3-flask
 pip install -r requirements.txt
