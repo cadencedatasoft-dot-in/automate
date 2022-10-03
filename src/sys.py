aa579611468685ada33fd88c64a2867d75408ce0
Cadence Data Soft <banand@cadencedatasoft.in>
2022-10-03 18:01:54 +0530

    Added VNC support

--- src/sys.py
+++ src/sys.py
@@ -7,5 +7,5 @@ class Sys:
   def spinInstance(self):
     retval = subprocess.run(["sudo chmod 777 ./file.img"], shell=True).returncode
     if retval == 0:
-      retval = subprocess.run(["qemu-system-x86_64 -nographic -curses -vga virtio -hda ./file.img -m 2G"], shell=True).returncode
+      retval = subprocess.run(["qemu-system-x86_64 -nographic -display curses -hda ./file.img -m 1G -vnc 0.0.0.0:0"], shell=True).returncode
     return retval
