2f965c84c2662993fa129fb02e1ae53ad0e5b89b
Cadence Data Soft <banand@cadencedatasoft.in>
2022-10-04 14:14:32 +0530

    Instance is now e

--- src/sys.py
+++ src/sys.py
@@ -15,7 +15,7 @@ class Sys:
   def spinInstance(self):
     retval = subprocess.run(["sudo chmod 777 ./file.img"], shell=True).returncode
     if retval == 0:
-      cmd = ["qemu-system-x86_64 -nographic -display curses -hda ./file.img -m 1G -vnc 0.0.0.0:0"]
+      cmd = ["qemu-system-x86_64 -hda ./file.img -m 1G -vnc 0.0.0.0:0 -daemonize"]
       p = Popen(
           cmd, shell=False, stdin=None, stdout=None, stderr=None,
           close_fds=True, creationflags=DETACHED_PROCESS )
