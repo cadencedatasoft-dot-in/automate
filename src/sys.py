5de7e20655f3e95b25f80303f5980a5052752423
Cadence Data Soft <banand@cadencedatasoft.in>
2022-10-04 13:09:51 +0530

    Nonblocking code

--- src/sys.py
+++ src/sys.py
@@ -1,11 +1,25 @@
 import subprocess
+from subprocess import Popen
+DETACHED_PROCESS = 0x00000008
 
 class Sys:
   def __init__(self):
     self.spinup = "qemu -m 2048 -hda ./alpine-first.img -m 1g"
 
-  def spinInstance(self):
+  def spinInstanceOld(self):
     retval = subprocess.run(["sudo chmod 777 ./file.img"], shell=True).returncode
     if retval == 0:
       retval = subprocess.run(["qemu-system-x86_64 -nographic -display curses -hda ./file.img -m 1G -vnc 0.0.0.0:0"], shell=True).returncode
+    return retval
+
+  def spinInstance(self):
+    retval = subprocess.run(["sudo chmod 777 ./file.img"], shell=True).returncode
+    if retval == 0:
+      cmd = ["qemu-system-x86_64 -nographic -display curses -hda ./file.img -m 1G -vnc 0.0.0.0:0"]
+      p = Popen(
+          cmd, shell=False, stdin=None, stdout=None, stderr=None,
+          close_fds=True, creationflags=DETACHED_PROCESS )
+      if p:
+        return 0
+    
     return retval
